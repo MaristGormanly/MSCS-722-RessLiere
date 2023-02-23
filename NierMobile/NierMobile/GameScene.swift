9fa231bd014b6e0bd5d95302cdd6917c0d5a0627
KyleRessLiere <kressliere@gmail.com>
2023-02-23 10:34:43 -0500

    inital scene with stars

--- NierMobile/NierMobile/GameScene.swift
+++ NierMobile/NierMobile/GameScene.swift
@@ -10,9 +10,26 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    var player:SKSpriteNode!
+    var starfield:SKEmitterNode!
     
     
     override func didMove(to view: SKView) {
+        //initlize startfield
+        starfield = SKEmitterNode(fileNamed: "Starfield")
+        
+        //initlize postion for starfield
+        //add later to detect for device
+        starfield.position = CGPoint(x:0, y: 1472)
+        //skip 10 seconds into animation
+        starfield.advanceSimulationTime(10)
+        //add starfield to screen
+        self.addChild(starfield)
+        //send starfield to back of screen
+        starfield.zPosition = -1
+        
+        
+        
         
        
     }
