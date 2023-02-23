0c87b55434089f4faa658d57623e590d8fc976a4
KyleRessLiere <kressliere@gmail.com>
2023-02-23 12:30:54 -0500

    rearranged scene according to new anchor point of 0 0  using self.frame

--- NierMobile/NierMobile/GameScene.swift
+++ NierMobile/NierMobile/GameScene.swift
@@ -8,11 +8,16 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
-    
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    // ! is optional meaning value does not have be defined right away
     var player:SKSpriteNode!
     var starfield:SKEmitterNode!
-    
+    var scoreLabel:SKLabelNode!
+    var score:Int = 0 {
+        didSet{
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
     
     override func didMove(to view: SKView) {
         //initlize startfield
@@ -20,7 +25,7 @@ class GameScene: SKScene {
         
         //initlize postion for starfield
         //TODO: add later to detect for device for placing start field
-        starfield.position = CGPoint(x:0, y: 1400)
+        starfield.position = CGPoint(x:view.frame.maxX, y: self.frame.height + 100)
         //skip 10 seconds into animation
         starfield.advanceSimulationTime(10)
         //add starfield to screen
@@ -35,12 +40,31 @@ class GameScene: SKScene {
         let centerX = view.frame.midX
         let centerY = view.frame.midY
         //set player intial postiion
-        player.position = CGPoint(x: centerX, y: centerY)
+        player.position = CGPoint(x: self.frame.size.width / 2, y: player.size.height / 2 + 20)
         
         //add player to screen
         self.addChild(player)
         
+        //set physics for scene zero gravity
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        //sets physics rules for contact
+        //TODO: contact delegate will be defined
+        self.physicsWorld.contactDelegate = self
         
+        
+        //set the score label inital
+        scoreLabel = SKLabelNode(text: "Score - 0")
+        //TODO: add as ratio of screen rather than hard coded
+        scoreLabel.position = CGPoint(x:self.frame.width / 2, y: self.frame.height - 100)
+        print(view.frame.maxY)
+        print(self.frame.size.height - 50)
+       print(-1*player.size.height/2 - 500)
+        //http://iosfonts.com/
+        scoreLabel.fontName = "Avenir-BlackOblique"
+        scoreLabel.fontSize = 48
+        scoreLabel.fontColor = UIColor.white
+        score = 0
+        self.addChild(scoreLabel)
         
         
         
