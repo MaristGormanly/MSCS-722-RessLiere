e0cfacc7c8f2d46323c3bf080355ddc0afadacf3
KyleRessLiere <kressliere@gmail.com>
2023-02-27 09:56:56 -0500

    added so top homescreen button is new Game

--- NierMobile/NierMobile/HomeScene.swift
+++ NierMobile/NierMobile/HomeScene.swift
@@ -14,13 +14,26 @@ import AVFoundation
 
 class HomeScene: SKScene {
     
-   
+    var newGameButtonNode:SKSpriteNode!
+    
+    override func didMove(to view: SKView) {
+        newGameButtonNode = self.childNode(withName: "newGameButton") as! SKSpriteNode
+        
+        
+        
+    }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        print("testing")
-        let transition = SKTransition.moveIn(with: .right, duration: 1)
-        let gameScene = GameScene(size: self.size)
-        self.view?.presentScene(gameScene,transition: transition)
-       
+        let touch = touches.first
+        guard let location = touch?.location(in: self) else { return }
+        let nodesArray = self.nodes(at: location)
+        if nodesArray.first?.name == "newGameButton" {
+            let transition = SKTransition.flipHorizontal(withDuration: 0.5)
+            let gameScene = GameScene(size: self.size)
+            self.view?.presentScene(gameScene, transition: transition)
+            
+            
+        }
     }
 }
 
