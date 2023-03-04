4856439e1f62370db45e6d3a6f507b6b15c7bcd7
KyleRessLiere <kressliere@gmail.com>
2023-03-04 15:03:11 -0500

    updated title screen

--- NierMobile/NierMobile/Scenes/HomeScene/HomeScene.swift
+++ NierMobile/NierMobile/Scenes/HomeScene/HomeScene.swift
@@ -13,13 +13,26 @@ import AVFoundation
 
 
 class HomeScene: SKScene {
-    
+    var gameTitleLabel:SKLabelNode!
     var newGameButtonNode:SKSpriteNode!
     var settingButtonNode:SKSpriteNode!
     var storyModeButtonNode:SKSpriteNode!
     var leaderboardButtonNode:SKSpriteNode!
     
     override func didMove(to view: SKView) {
+        //set the score label inital
+        let titleColor = UIColor(red: 0.79, green: 0.85, blue: 0.32, alpha: 1.0)
+
+        gameTitleLabel = SKLabelNode(text: "NieR: Mobile")
+        gameTitleLabel.zRotation = -1*CGFloat.pi / 2.0
+        
+        gameTitleLabel.position = CGPoint(x:self.frame.width * 0.76 , y: self.frame.height / 2)
+    
+        //http://iosfonts.com/
+        gameTitleLabel.fontName = "Copperplate"
+        gameTitleLabel.fontSize = 84
+        gameTitleLabel.fontColor = titleColor
+        addChild(gameTitleLabel)
        newGameButtonNode = SKSpriteNode(imageNamed: "start-button")
         newGameButtonNode.position = CGPoint(x:self.frame.size.width * 0.55, y: self.frame.height / 2)
         newGameButtonNode.zRotation =  -1*CGFloat.pi / 2.0
@@ -40,8 +53,9 @@ class HomeScene: SKScene {
         addChild(storyModeButtonNode)
         
         leaderboardButtonNode = SKSpriteNode(imageNamed: "leaderboard-button")
-        leaderboardButtonNode.setScale(0.8)
-        leaderboardButtonNode.position = CGPoint(x:self.frame.size.width * 0.18, y: self.frame.height * 0.2)
+        leaderboardButtonNode.setScale(0.82)
+        leaderboardButtonNode.position = CGPoint(x:self.frame.size.width * 0.185, y: self.frame.height * 0.19
+        )
         leaderboardButtonNode.zRotation = -1*CGFloat.pi / 2.0
         leaderboardButtonNode.name = "leaderboardButton"
         addChild(leaderboardButtonNode)
