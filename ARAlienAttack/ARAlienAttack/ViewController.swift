3babb5d2d58322d1c95a1132085ae3f830613f67
KyleRessLiere <kressliere@gmail.com>
2023-05-06 19:16:24 -0400

    Inital scene view

--- ARAlienAttack/ARAlienAttack/ViewController.swift
+++ ARAlienAttack/ARAlienAttack/ViewController.swift
@@ -15,18 +15,14 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // Set the view's delegate
-        sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+          sceneView.delegate = self
+          // Show statistics such as fps and timing information
+          sceneView.showsStatistics = true
+          // Create a new scene
+          let scene = SCNScene(named: "art.scnassets/scene.scn")!
+          // Set the scene to the view
+          sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
